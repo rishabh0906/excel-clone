8d604ca82edaff1be16c78a7f747dc9d725ed94c
rishabh <rishabhj945@gmail.com>
2021-07-31 15:42:42 +0530

    changes

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//hello
 let body = document.querySelector("body");
 let dataObj = {};
 body.spellcheck = false;
@@ -7,17 +6,16 @@ let rowNumbers = document.querySelector(".row-numbers");
 let grid = document.querySelector(".grid");
 let formulaSelectCell = document.querySelector("#select-cell");
 let formulaCell = document.querySelector("#complete-formula");
- menuBarPtags = document.querySelectorAll(".menu-bar p");
+let menuBarPtags = document.querySelectorAll(".menu-bar p");
 
 for (let i = 0; i < menuBarPtags.length; i++) {
   menuBarPtags[i].addEventListener("click", function (e) {
     if (e.currentTarget.classList.contains("menu-bar-option-selected")) {
       e.currentTarget.classList.remove("menu-bar-option-selected");
     } else {
-
       for (let j = 0; j < menuBarPtags.length; j++) {
         if (menuBarPtags[j].classList.contains("menu-bar-option-selected"))
-          menuBarPtags[j].classList.remove("menu-bar-option-selected")
+          menuBarPtags[j].classList.remove("menu-bar-option-selected");
       }
 
       e.currentTarget.classList.add("menu-bar-option-selected");
@@ -53,14 +51,14 @@ for (let j = 1; j <= 100; j++) {
       formula: "",
       downstream: [],
       upstream: [],
-      fontSize:10,
-      fontFamily:"Arial",
-      fontWeight:"normal",
-      color:"black",
-      backgroundColor:"white",
-      underline:"none",
-      italics:"normal",
-      textAlign:"left"
+      fontSize: 10,
+      fontFamily: "Arial",
+      fontWeight: "normal",
+      color: "black",
+      backgroundColor: "white",
+      underline: "none",
+      italics: "normal",
+      textAlign: "left",
     };
     cell.addEventListener("click", (e) => {
       if (oldCell) {
@@ -72,12 +70,10 @@ for (let j = 1; j <= 100; j++) {
 
       formulaSelectCell.value = cellAddress;
 
-
       oldCell = e.currentTarget;
-
+      
     });
     cell.addEventListener("input", (e) => {
-
       let data = e.currentTarget.innerText;
       let addr = e.currentTarget.getAttribute("data-address");
       dataObj[addr].value = data;
@@ -94,18 +90,134 @@ for (let j = 1; j <= 100; j++) {
       for (let i = 0; i < currCellDownstream.length; i++) {
         updateDownstreamElements(currCellDownstream[i]);
       }
-
-
     });
     row.append(cell);
   }
   grid.append(row);
 }
 
-formulaCell.addEventListener("change", (e) => {
 
+formulaCell.addEventListener("change", (e) => {
   let formulae = e.currentTarget.value;
-  let selectedCellAddress = oldCell.getAttribute("data-address");
+  AddFormula(formulae);
+  formulaCell.value = "";
+});
+function addToDownstream(tobeAdded, inWhichWeAreAdding) {
+  //get downstream of the cell in which we have to add
+  let reqDownstream = dataObj[inWhichWeAreAdding].downstream;
+
+  reqDownstream.push(tobeAdded);
+}
+
+function removeFromUpstream(dependent, onWhichItIsDepending) {
+  let newDownstream = [];
+
+  let oldDownstream = dataObj[onWhichItIsDepending].downstream;
+
+  for (let i = 0; i < oldDownstream.length; i++) {
+    if (oldDownstream[i] != dependent) newDownstream.push(oldDownstream[i]);
+  }
+  dataObj[onWhichItIsDepending].downstream = newDownstream;
+}
+
+function updateDownstreamElements(elementAddress) {
+  //1- jis element ko update kr rhe hai unki upstream elements ki current value leao
+  //unki upstream ke elements ka address use krke dataObj se unki value lao
+  //unhe as key value pair store krdo valObj naam ke obj me
+  let valObj = {};
+
+  let currCellUpstream = dataObj[elementAddress].upstream;
+
+  for (let i = 0; i < currCellUpstream.length; i++) {
+    let upstreamCellAddress = currCellUpstream[i];
+    let upstreamCellValue = dataObj[upstreamCellAddress].value;
+
+    valObj[upstreamCellAddress] = upstreamCellValue;
+  }
+
+  //2- jis element ko update kr rhe hai uska formula leao
+  let currFormula = dataObj[elementAddress].formula;
+  //formula ko space ke basis pr split maro
+  let formulaArr = currFormula.split(" ");
+  //split marne ke baad jo array mili uspr loop ara and formula me jo variable h(cells) unko unki value se replace krdo using valObj
+  for (let j = 0; j < formulaArr.length; j++) {
+    if (valObj[formulaArr[j]]) {
+      formulaArr[j] = valObj[formulaArr[j]];
+    }
+  }
+
+  //3- Create krlo wapis formula from the array by joining it
+  currFormula = formulaArr.join(" ");
+
+  //4- evaluate the new value using eval function
+  let newValue = eval(currFormula);
+
+  //update the cell(jispr function call hua) in dataObj
+  dataObj[elementAddress].value = newValue;
+
+  //5- Ui pr update krdo new value
+  let cellOnUI = document.querySelector(`[data-address=${elementAddress}]`);
+  cellOnUI.innerText = newValue;
+
+  //6- Downstream leke ao jis element ko update kra just abhi kuki uspr bhi kuch element depend kr sakte hai
+  //unko bhi update krna padega
+  let currCellDownstream = dataObj[elementAddress].downstream;
+
+  //check kro ki downstream me elements hai kya agr han to un sab pr yehi function call krdo jise wo bhi update hojai with new value
+  if (currCellDownstream.length > 0) {
+    for (let k = 0; k < currCellDownstream.length; k++) {
+      updateDownstreamElements(currCellDownstream[k]);
+    }
+  }
+}
+
+// =============================================== Detect Cycle ========================================
+function cycle( cellAddress, formulaeArray){
+
+
+  let downstream=dataObj[cellAddress].downstream;
+
+if(formulaeArray.find((el)=>{
+
+return cellAddress==el;
+
+}))
+{
+  return true;
+}
+
+
+  for(let i=0;i<downstream.length;i++)
+  {
+    for(let j=0;j<formulaeArray.length;j++)
+    {
+      if(formulaeArray[j]==downstream[i])
+      {
+        //cycle Detected//
+        return true;
+      }
+    }
+  }
+
+  for(let i=0;i<downstream.length;i++)
+  {
+
+    if(cycle(downstream[i],formulaeArray))
+    {
+      return true;
+    }
+  }
+
+  return false;
+
+}
+
+// ==================================================== ADD Formula ===============================================
+
+function AddFormula(formulae)
+{
+  let selectedCellAddress = oldCell.getAttribute("data-Address");
+  
 
   dataObj[selectedCellAddress].formula = formulae;
 
@@ -127,10 +239,15 @@ formulaCell.addEventListener("change", (e) => {
 
   //BEFORE SETTING NEW UPSTREAM
   //CLEAR OLD UPSTREAM
+  if(cycle(selectedCellAddress,elementsArray))
+  {
+    formulaCell.value="";
+    alert("Circular Dependencies!! ");
+    showErr(selectedCellAddress);
+     return ;
+  }  
 
   let oldUpstream = dataObj[selectedCellAddress].upstream;
-
-
 
   for (let k = 0; k < oldUpstream.length; k++) {
     removeFromUpstream(selectedCellAddress, oldUpstream[i]);
@@ -167,79 +284,18 @@ formulaCell.addEventListener("change", (e) => {
   }
 
   oldCell.innerText = newValue;
-  formulaCell.value = ""
-
-
-});
-function addToDownstream(tobeAdded, inWhichWeAreAdding) {
-  //get downstream of the cell in which we have to add
-  let reqDownstream = dataObj[inWhichWeAreAdding].downstream;
-
-  reqDownstream.push(tobeAdded);
 }
 
-function removeFromUpstream(dependent, onWhichItIsDepending) {
-  let newDownstream = [];
+function showErr(cellAddress)
+{
 
-  let oldDownstream = dataObj[onWhichItIsDepending].downstream;
+dataObj[cellAddress].value="err!!";
+document.querySelector(`div[data-address=${cellAddress}]`).innerHTML="err!!";
 
-
-  for (let i = 0; i < oldDownstream.length; i++) {
-    if (oldDownstream[i] != dependent) newDownstream.push(oldDownstream[i]);
-  }
-  dataObj[onWhichItIsDepending].downstream = newDownstream;
+let downStream=dataObj[cellAddress].downstream;
+for(let i=0;i<downStream.length;i++)
+{
+  showErr(downStream[i]);
 }
 
-function updateDownstreamElements(elementAddress) {
-  //1- jis element ko update kr rhe hai unki upstream elements ki current value leao
-  //unki upstream ke elements ka address use krke dataObj se unki value lao 
-  //unhe as key value pair store krdo valObj naam ke obj me
-  let valObj = {};
-
-  let currCellUpstream = dataObj[elementAddress].upstream;
-
-  for (let i = 0; i < currCellUpstream.length; i++) {
-    let upstreamCellAddress = currCellUpstream[i];
-    let upstreamCellValue = dataObj[upstreamCellAddress].value;
-
-    valObj[upstreamCellAddress] = upstreamCellValue;
-
-  }
-
-  //2- jis element ko update kr rhe hai uska formula leao
-  let currFormula = dataObj[elementAddress].formula;
-  //formula ko space ke basis pr split maro
-  let formulaArr = currFormula.split(" ");
-  //split marne ke baad jo array mili uspr loop ara and formula me jo variable h(cells) unko unki value se replace krdo using valObj
-  for (let j = 0; j < formulaArr.length; j++) {
-    if (valObj[formulaArr[j]]) {
-      formulaArr[j] = valObj[formulaArr[j]];
-    }
-  }
-
-
-  //3- Create krlo wapis formula from the array by joining it 
-  currFormula = formulaArr.join(" ");
-
-  //4- evaluate the new value using eval function 
-  let newValue = eval(currFormula);
-
-
-  //update the cell(jispr function call hua) in dataObj
-  dataObj[elementAddress].value = newValue;
-
-  //5- Ui pr update krdo new value 
-  let cellOnUI = document.querySelector(`[data-address=${elementAddress}]`);
-  cellOnUI.innerText = newValue;
-
-  //6- Downstream leke ao jis element ko update kra just abhi kuki uspr bhi kuch element depend kr sakte hai
-  //unko bhi update krna padega
-  let currCellDownstream = dataObj[elementAddress].downstream;
-
-  //check kro ki downstream me elements hai kya agr han to un sab pr yehi function call krdo jise wo bhi update hojai with new value 
-  if (currCellDownstream.length > 0) {
-    for (let k = 0; k < currCellDownstream.length; k++) {
-      updateDownstreamElements(currCellDownstream[k]);
-    }
-  }
 }
--- styleScript.js
+++ styleScript.js
@@ -7,124 +7,84 @@ let decrease = document.querySelector(".decrease");
 
 let currentSize = document.querySelector(".current");
 
-
-
 decorationSpans[0].addEventListener("click", (e) => {
-
-    oldCell.style.fontWeight = "bold";
-    let cellAdress = oldCell.getAttribute("data-address");
-    dataObj[cellAdress].fontWeight = "bold";
+  oldCell.style.fontWeight = "bold";
+  let cellAdress = oldCell.getAttribute("data-address");
+  dataObj[cellAdress].fontWeight = "bold";
 });
 
 decorationSpans[1].addEventListener("click", (e) => {
-    oldCell.style.fontStyle = "Italic";
-    let cellAdress = oldCell.getAttribute("data-address");
-    dataObj[cellAdress].italics = "italic";
-
+  oldCell.style.fontStyle = "Italic";
+  let cellAdress = oldCell.getAttribute("data-address");
+  dataObj[cellAdress].italics = "italic";
 });
 
 decorationSpans[2].addEventListener("click", (e) => {
-    oldCell.style.textDecoration = "underline";
-    let cellAdress = oldCell.getAttribute("data-address");
-    dataObj[cellAdress].underline = "underline";
-
+  oldCell.style.textDecoration = "underline";
+  let cellAdress = oldCell.getAttribute("data-address");
+  dataObj[cellAdress].underline = "underline";
 });
 
 alignmentSpans[0].addEventListener("click", (e) => {
-    oldCell.style.textAlign = "left";
-    let cellAdress = oldCell.getAttribute("data-address");
-    dataObj[cellAdress].textAlign = "left";
-
-
-
+  oldCell.style.textAlign = "left";
+  let cellAdress = oldCell.getAttribute("data-address");
+  dataObj[cellAdress].textAlign = "left";
 });
 alignmentSpans[1].addEventListener("click", (e) => {
-
-    oldCell.style.textAlign = "center";
-    let cellAdress = oldCell.getAttribute("data-address");
-    dataObj[cellAdress].textAlign = "center";
-
-
+  oldCell.style.textAlign = "center";
+  let cellAdress = oldCell.getAttribute("data-address");
+  dataObj[cellAdress].textAlign = "center";
 });
 alignmentSpans[2].addEventListener("click", (e) => {
-
-    oldCell.style.textAlign = "right";
-    let cellAdress = oldCell.getAttribute("data-address");
-    dataObj[cellAdress].textAlign = "right";
-
-
+  oldCell.style.textAlign = "right";
+  let cellAdress = oldCell.getAttribute("data-address");
+  dataObj[cellAdress].textAlign = "right";
 });
 
 colorSpans[0].addEventListener("click", function () {
-    let colorPicker = document.createElement("input");
-    colorPicker.type = "color";
+  let colorPicker = document.createElement("input");
+  colorPicker.type = "color";
 
-    colorPicker.addEventListener("change", function (e) {
-        oldCell.style.color = e.currentTarget.value;
-        let address = oldCell.getAttribute("data-address");
-        dataObj[address].fontColor = e.currentTarget.value;
-    });
+  colorPicker.addEventListener("change", function (e) {
+    oldCell.style.color = e.currentTarget.value;
+    let address = oldCell.getAttribute("data-address");
+    dataObj[address].fontColor = e.currentTarget.value;
+  });
 
-    colorPicker.click();
+  colorPicker.click();
 });
 
 colorSpans[1].addEventListener("click", function () {
-    let colorPicker = document.createElement("input");
-    colorPicker.type = "color";
+  let colorPicker = document.createElement("input");
+  colorPicker.type = "color";
 
-    colorPicker.addEventListener("change", function (e) {
-        oldCell.style.backgroundColor = e.currentTarget.value;
-        let address = oldCell.getAttribute("data-address");
-        dataObj[address].backgroundColor = e.currentTarget.value;
+  colorPicker.addEventListener("change", function (e) {
+    oldCell.style.backgroundColor = e.currentTarget.value;
+    let address = oldCell.getAttribute("data-address");
+    dataObj[address].backgroundColor = e.currentTarget.value;
+  });
 
-
-    });
-
-    colorPicker.click();
+  colorPicker.click();
 });
 
-
 font_family.addEventListener("click", (e) => {
-    oldCell.style.fontFamily = e.currentTarget.value;
-    let address = oldCell.getAttribute("data-address");
-    dataObj[address].fontFamily = e.currentTarget.value;
-
+  oldCell.style.fontFamily = e.currentTarget.value;
+  let address = oldCell.getAttribute("data-address");
+  dataObj[address].fontFamily = e.currentTarget.value;
 });
 
 increase.addEventListener("click", (e) => {
-
-    let curr = Number(currentSize.innerText);
-    if (curr < 50) {
-        currentSize.innerText = curr + 1;
-        oldCell.style.fontSize = `${curr + 1}px`;
-        let address = oldCell.getAttribute("data-address");
-        dataObj[address].fontSize = curr + 1;
-
-    }
-
+  let curr = Number(currentSize.innerText);
+  if (curr < 50) {
+    currentSize.innerText = curr + 1;
+    oldCell.style.fontSize = `${curr + 1}px`;
+  }
 });
 decrease.addEventListener("click", (e) => {
+  let curr = Number(currentSize.innerText);
 
-    let curr = Number(currentSize.innerText);
-
-    if (curr > 0) {
-        currentSize.innerText = curr - 1;
-        oldCell.style.fontSize = `${curr - 1}px`;
-        let address = oldCell.getAttribute("data-address");
-        dataObj[address].fontSize = curr - 1;
-
-
-    }
-});
-
-
-grid.addEventListener("scroll", function (e) {
-
-    let currDistanceFromTop = e.currentTarget.scrollTop; //vertical
-    let currDistanceFromleft = e.currentTarget.scrollLeft;
-
-    columnTags.style.transform = `translateX(-${currDistanceFromleft}px)`;
-
-    rowNumbers.style.transform = `translateY(-${currDistanceFromTop}px)`;
-
+  if (curr > 0) {
+    currentSize.innerText = curr - 1;
+    oldCell.style.fontSize = `${curr - 1}px`;
+  }
 });
